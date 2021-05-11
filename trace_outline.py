2ac633bf7baa968db6ace28ca9105f937173321f
Huangtingting93 <80449377+Huangtingting93@users.noreply.github.com>
2021-05-11 19:50:51 +0800

    fix bugs in clockwise

--- trace_outline.py
+++ trace_outline.py
@@ -2,7 +2,6 @@
 import numpy as np
 NORM = np.linalg.norm
 from pdb import set_trace as bp
-
 def get_neibour(id,f):
     neibour_list = []
     for i in id:
@@ -46,7 +45,7 @@ def find_clockwise_nearest(vector_a,vector_b_arr,id_list):
     """
     ang = np.arctan2(vector_a[0]*vector_b_arr[:,1]-vector_a[1]*vector_b_arr[:,0],vector_a[0]*vector_b_arr[:,0]+vector_a[1]*vector_b_arr[:,1])
     # id_list = vector_b_arr[:,2]
-    positive_id = np.where(ang > 1e-12)[0] # when ang == 0, means vector_a find it self.  using 1e-12 to aviod float precision error,
+    positive_id = np.where(ang > 1e-8)[0] # when ang == 0, means vector_a find it self.  using 1e-8 to aviod float precision error,
     if positive_id.shape[0] > 0 : 
         # e.g angle [-20,20,30] we wanna get 20 degree, rather than -20 degree, 
         # because -20 degree means the vector has neg direction compare to vector_a
@@ -57,64 +56,6 @@ def find_clockwise_nearest(vector_a,vector_b_arr,id_list):
     next_pt_id = int(id_list[clockwise_nearest])
     return next_pt_id
 
-def build_search_circle(verts,faces,length_threshold = 30):
-    # length_threshold can be adjust according to mesh size
-    edges_list,adj = get_edges(verts,faces)
-    edge_arr = np.asarray(edges_list)
-    out_points = []
-    connect_id = []
-    tree = cKDTree(verts)
-    length = np.max(verts)-np.min(verts)
-    with_in_circle = tree.query_ball_point(verts,length/length_threshold)
-    search_area_list = []
-    for idx in range(verts.shape[0]):
-        search_area_list.append(np.unique( np.vstack(( edge_arr[with_in_circle[idx]] )),axis=0 ))
-    return search_area_list
-
-def tracing_outline(verts,faces,search_area_list):
-    """
-    args:
-    1.N*2 verts
-    2.M*2 edges
-    Return:
-    outline points coordinates
-    """
-    start_id = np.argmin(verts[:,0])
-    center_pt = verts[start_id]
-    pre_pt = center_pt.copy()
-    pre_pt[0] = pre_pt[0] - 1 #start from left
-    next_id = start_id
-    break_c = verts.shape[0] 
-    i = 0
-    out_id = []
-    out_id.append(next_id)
-    while True and i < break_c:
-        i += 1
-        if len(connect_id) == 0:
-            connect_id = adj[next_id]
-        vector_a = pre_pt - center_pt
-        vector_b_arr = verts[connect_id] - center_pt
-        next_id = find_clockwise_nearest(vector_a,vector_b_arr,connect_id)
-        if next_id == start_id:
-            break
-        pre_pt = center_pt
-        center_pt = verts[next_id]
-        search_area = search_area_list[next_id]
-        arr_q = verts[search_area[:,0]]
-        arr_r = verts[search_area[:,1]] - arr_q
-        inters,inter_edge_id = find_inters(center_pt,pre_pt-center_pt,arr_q,arr_r)
-        if inters is not None:
-            nearest = np.argmin(NORM(inters - pre_pt,axis=1))
-            center_pt = inters[nearest]
-            connect_id = np.ndarray.tolist(search_area[inter_edge_id[nearest]])
-            connect_id.append(next_id)
-            inters = None  
-        else:
-            connect_id = []     
-            inters = None
-            out_id.append(next_id)
-        out_points.append(center_pt)    
-    return out_id
 
 def find_inters(pv,rv,qv,sv):
     # find intersections https://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
@@ -127,7 +68,7 @@ def find_inters(pv,rv,qv,sv):
     qv_minus_pv = qv - pv
     t = (qv_minus_pv[:,0]*sv[:,1]-qv_minus_pv[:,1]*sv[:,0]) / cross
     u = (qv_minus_pv[:,0]*rv[1]-qv_minus_pv[:,1]*rv[0]) / cross
-    line_has_inters = np.where( ( (t < 1-1e-6) & (t>1e-6))  & ( (u<1-1e-6) & (u>1e-6)) & (cross !=0) )[0]
+    line_has_inters = np.where( ( (t < 1-1e-12) & (t>1e-12))  & ( (u<1-1e-12) & (u>1e-12)) & (cross !=0) )[0]
     if line_has_inters.shape[0] !=0:
         intersections = pv + t[line_has_inters].reshape(-1,1) * rv
         return intersections,line_has_inters
@@ -158,11 +99,16 @@ def tracing_outline_robust(verts,faces):
     out_id = []
     out_id.append(next_id)
     out_points.append(verts[next_id])
+
     while True and i < break_c:
         i += 1
         if len(connect_id) == 0:
             connect_id = adj[next_id]
         vector_a = pre_pt - center_pt
+        vector_b_arr = verts[connect_id] - center_pt
+        # filter point in connect_id that lies in vector_a
+        not_lie_in_vector_a = np.where(np.abs(np.cross(vector_b_arr,vector_a)) > 1e-10)[0]
+        connect_id = np.ndarray.tolist(np.asarray(connect_id)[not_lie_in_vector_a])
         vector_b_arr = verts[connect_id] - center_pt
         next_id = find_clockwise_nearest(vector_a,vector_b_arr,connect_id)
         if next_id == start_id:
@@ -172,11 +118,23 @@ def tracing_outline_robust(verts,faces):
         arr_q = verts[edge_arr[:,0]]
         arr_r = verts[edge_arr[:,1]] - arr_q
         inters,inter_edge_id = find_inters(center_pt,pre_pt-center_pt,arr_q,arr_r)
+        # double check, filter inters that is actually the node of the edge
+        if inters is not None:
+            inters_filter = [] 
+            for j,inter in enumerate(inters):
+                if NORM(inter-center_pt) < 1e-12 or NORM(inter-pre_pt) < 1e-12:
+                    continue
+                else:
+                    inters_filter.append(inter)
+            if len(inters_filter) > 0:
+                inters = np.vstack(inters_filter)
+            else:
+                inters = None
+
         if inters is not None:
             nearest = np.argmin(NORM(inters - pre_pt,axis=1))
             center_pt = inters[nearest]
             connect_id = np.ndarray.tolist(edge_arr[inter_edge_id[nearest]])
-            connect_id.append(next_id)
             inters = None  
         else:
             connect_id = []     
@@ -184,6 +142,7 @@ def tracing_outline_robust(verts,faces):
             out_id.append(next_id)      
         out_points.append(center_pt)    
     return np.asarray(out_points),out_id
+
 
 
 import matplotlib.pyplot as plt
